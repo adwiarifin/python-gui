5abbda82c14522b4ce819ab558e94040e6e00754
Adwi Arifin <adwiarifin@gmail.com>
2019-05-06 07:29:30 +0700

    finishing section01

---
+++ section01/06_gui_set_focus.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from tkinter import ttk
+
+win = tk.Tk()
+
+win.title("Python GUI")
+
+def click_me():
+	action.configure(text="Hello " + name.get())
+
+ttk.Label(win, text="Enter a name:").grid(column=0, row=0)
+
+name = tk.StringVar()
+name_entered = ttk.Entry(win, width=12, textvariable=name)
+name_entered.grid(column=0, row=1)
+
+action = ttk.Button(win, text="Click Me!", command=click_me)
+action.grid(column=1, row=1)
+
+name_entered.focus()
+
+#===========================
+# Start GUI
+#===========================
+win.mainloop()
